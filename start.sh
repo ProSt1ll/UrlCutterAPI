eb9493371e4e2530be5e5f168d823b99354a3510
ProSt1ll <gubilnurr@gmail.com>
2022-12-13 16:50:32 +0300

    fix start script

--- start.sh
+++ start.sh
@@ -3,6 +3,6 @@ docker-compose down --volumes
 docker-compose up --build --no-start
 if [ "$#" -lt 1 ]; then
 
-    docker-compose run -e SaveMethod=$1 urlcutterapi
+    docker-compose run -e SaveMethod=postgres urlcutterapi
 fi
 docker-compose run -e SaveMethod=$1 urlcutterapi
